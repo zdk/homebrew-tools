f248ad8826b14179fea8cc40338fd23df0a6293a
zdk <di.warachet@gmail.com>
2025-07-02 21:04:22 +0700

    docs: add caveats

--- Formula/rm-safely.rb
+++ Formula/rm-safely.rb
@@ -1,32 +1,38 @@
 class RmSafely < Formula
-  desc 'Safe rm command that backs up files to trash before deletion'
-  homepage 'https://github.com/zdk/rm-safely'
-  url 'https://github.com/zdk/rm-safely/archive/refs/heads/main.tar.gz'
-  version '1.0.7'
-  sha256 '5f901c58c06532711215fadd69738312833aa3c04ea9fef20d92511c8ea287b5'
-  license 'MIT'
+  desc "Safe rm command that backs up files to trash before deletion"
+  homepage "https://github.com/zdk/rm-safely"
+  url "https://github.com/zdk/rm-safely/archive/refs/heads/main.tar.gz"
+  version "1.0.7"
+  sha256 "5f901c58c06532711215fadd69738312833aa3c04ea9fef20d92511c8ea287b5"
+  license "MIT"
 
   def install
-    bin.install 'rm-safely.sh' => 'rm-safely'
+    bin.install "rm-safely.sh" => "rm-safely"
 
-    # Create a wrapper script for easier integration
-    (bin / 'rm-safely-install').write <<~EOS
+    (bin / "rm-safely-install").write <<~EOS
       #!/bin/bash
       exec "#{bin}/rm-safely" install "$@"
     EOS
 
-    (bin / 'rm-safely-uninstall').write <<~EOS
+    (bin / "rm-safely-uninstall").write <<~EOS
       #!/bin/bash
       exec "#{bin}/rm-safely" uninstall "$@"
     EOS
 
-    chmod 0o755, bin / 'rm-safely-install'
-    chmod 0o755, bin / 'rm-safely-uninstall'
+    chmod 0o755, bin / "rm-safely-install"
+    chmod 0o755, bin / "rm-safely-uninstall"
   end
 
   def caveats
     <<~EOS
-      Files deleted with 'rm' will be moved to ~/.local/share/Trash
+      To enable the 'rm' command override, run:
+        rm-safely install
+
+      Then reload your shell:
+        source ~/.zshrc  # for zsh
+        source ~/.bashrc # for bash
+
+      After installation, files deleted with 'rm' will be moved to ~/.local/share/Trash
 
       Useful commands:
         rm --list-trash      # Show trash contents
@@ -34,11 +40,11 @@ class RmSafely < Formula
         rm --empty-trash     # Clear trash directory
         rm --rm file.txt     # Permanently delete (bypass trash)
         rm --help            # Show all options
-
+        rm --version         # Show version information
     EOS
   end
 
   test do
-    system "#{bin}/rm-safely", 'status'
+    system "#{bin}/rm-safely", "status"
   end
 end
