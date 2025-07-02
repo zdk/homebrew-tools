a9bc5ba86dd7e29d74e51c078ae8d95f8d1a0136
zdk <di.warachet@gmail.com>
2025-07-02 22:17:56 +0700

    chore: bump version

--- Formula/rm-safely.rb
+++ Formula/rm-safely.rb
@@ -1,26 +1,26 @@
 class RmSafely < Formula
-  desc "Safe rm command that backs up files to trash before deletion"
-  homepage "https://github.com/zdk/rm-safely"
-  url "https://github.com/zdk/rm-safely/archive/refs/heads/main.tar.gz"
-  version "1.0.8"
-  sha256 "fae4767ae6f8949acb36d06e78482822d77f8918b31f999d962a3fab7336986f"
-  license "MIT"
+  desc 'Safe rm command that backs up files to trash before deletion'
+  homepage 'https://github.com/zdk/rm-safely'
+  url 'https://github.com/zdk/rm-safely/archive/refs/heads/main.tar.gz'
+  version '1.1.1'
+  sha256 '57bef7e3ff0c9c070b5556ede15f000a6d33a982786726fb2f8d33b73b2cc157'
+  license 'MIT'
 
   def install
-    bin.install "rm-safely.sh" => "rm-safely"
+    bin.install 'rm-safely.sh' => 'rm-safely'
 
-    (bin / "rm-safely-install").write <<~EOS
+    (bin / 'rm-safely-install').write <<~EOS
       #!/bin/bash
       exec "#{bin}/rm-safely" install "$@"
     EOS
 
-    (bin / "rm-safely-uninstall").write <<~EOS
+    (bin / 'rm-safely-uninstall').write <<~EOS
       #!/bin/bash
       exec "#{bin}/rm-safely" uninstall "$@"
     EOS
 
-    chmod 0o755, bin / "rm-safely-install"
-    chmod 0o755, bin / "rm-safely-uninstall"
+    chmod 0o755, bin / 'rm-safely-install'
+    chmod 0o755, bin / 'rm-safely-uninstall'
   end
 
   def caveats
@@ -38,6 +38,6 @@ class RmSafely < Formula
   end
 
   test do
-    system "#{bin}/rm-safely", "status"
+    system "#{bin}/rm-safely", 'status'
   end
 end
